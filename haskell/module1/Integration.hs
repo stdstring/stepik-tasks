25c74168a877715a9b65e791c372aeb49152451f
stdstring <std_string@mail.ru>
2020-01-07 10:39:07 +0500

    made cleanup

--- haskell/module1/Integration.hs
+++ haskell/module1/Integration.hs
@@ -1,7 +1,6 @@
 integration :: (Double -> Double) -> Double -> Double -> Double
-integration f a b = h * (0.5 * (f a + f b) + innerSum 1 0)
-    where
-        n = 1000
-        h = (b - a) / n
-        innerSum i value | i == n = value
-                         | otherwise = innerSum (i + 1) (value + f (a + i * h))
+integration f a b = h * (0.5 * (f a + f b) + innerSum 1 0) where
+    n = 1000
+    h = (b - a) / n
+    innerSum i value | i == n = value
+                     | otherwise = innerSum (i + 1) (value + f (a + i * h))
--- haskell/module1/SeqA.hs
+++ haskell/module1/SeqA.hs
@@ -4,5 +4,5 @@ seqA 1 = 2
 seqA 2 = 3
 seqA n = let
              helper i n a2 a1 a0 | i == n = a2
-             helper i n a2 a1 a0 | otherwise = helper (i + 1) n (a2 + a1 - 2 * a0) a2 a1
+                                 | otherwise = helper (i + 1) n (a2 + a1 - 2 * a0) a2 a1
          in helper 2 n 3 2 1
